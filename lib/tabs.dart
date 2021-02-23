1832beb48057be1dc8efde51b5f555dcab23f6b0
your username <tho.nguyend2@homecredit.vn>
2021-02-23 16:48:24 +0700

    Add pageview

---
+++ lib/tabs.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class FirstTab extends StatefulWidget {
+  String title;
+  MaterialColor color;
+
+  FirstTab({Key key, this.title, this.color}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FirstTabState();
+  }
+}
+
+class _FirstTabState extends State<FirstTab> with AutomaticKeepAliveClientMixin<FirstTab> {
+  @override
+  void initState() {
+    // Future.delayed(Duration(microseconds: 2000), () {
+    //   Fluttertoast.showToast(msg: widget.title);
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Fluttertoast.showToast(msg: widget.title);
+    return Container(color: widget.color);
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
