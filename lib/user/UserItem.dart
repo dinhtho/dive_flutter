a54b439ce4e5cb2c7c6631203d9214ef4c95de81
your username <tho.nguyend2@homecredit.vn>
2021-02-26 18:04:48 +0700

    Update code

--- lib/user/UserItem.dart
+++ lib/user/UserItem.dart
@@ -13,7 +13,9 @@ class _UserItemState extends State<UserItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.yellow,
       padding: EdgeInsets.only(top: 8, left: 5, bottom: 8, right: 5),
+      margin: EdgeInsets.only(bottom: 10),
       child: Row(
         mainAxisSize: MainAxisSize.max,
         children: [
@@ -22,65 +24,59 @@ class _UserItemState extends State<UserItem> {
             backgroundImage: NetworkImage(
                 "https://464697-1455876-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/05/Vanja_Bertalan_Human_CEO_1-750x375.jpg"),
           ),
-          Container(
-            margin: EdgeInsets.only(left: 5),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Jon Street",
-                      style: ThemeText.bold.copyWith(fontSize: 16),
-                    ),
-                    Text(
-                      "3 hrs ago",
-                      textAlign: TextAlign.end,
-                      style: ThemeText.bold.copyWith(color: Colors.grey),
-                    ),
-                    // Align(
-                    //   alignment: Alignment.center,
-                    //   child: Text(
-                    //     "3 hrs ago",
-                    //     textAlign: TextAlign.end,
-                    //     style: ThemeText.bold.copyWith(color: Colors.grey),
-                    //   ),
-                    // )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Reputation",
-                      style: ThemeText.bold.copyWith(color: Colors.grey),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 3),
-                      child: Text(
-                        "1234454",
-                        style: ThemeText.bold,
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(left: 5),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Jon Street",
+                        style: ThemeText.bold.copyWith(fontSize: 16),
                       ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Location",
-                      style: ThemeText.bold.copyWith(color: Colors.grey),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 3),
-                      child: Text(
-                        "Reading,UK",
-                        style: ThemeText.bold,
+                      Text(
+                        "3 hrs ago",
+                        textAlign: TextAlign.end,
+                        style: ThemeText.bold.copyWith(color: Colors.grey),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Reputation",
+                        style: ThemeText.bold.copyWith(color: Colors.grey),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 3),
+                        child: Text(
+                          "1234454",
+                          style: ThemeText.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Location",
+                        style: ThemeText.bold.copyWith(color: Colors.grey),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 3),
+                        child: Text(
+                          "Reading,UK",
+                          style: ThemeText.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           )
         ],
